c531b5e6def12d8ef734e54cb12248a6cf20d6eb
Kunal Chachane <kunalchachane59@gmail.com>
2026-04-14 09:32:27 +0530

    String mastery

---
+++ String.py
@@ -0,0 +1,20 @@
+Str = "kunal Chachane is learning Python!"
+
+# Reversing a String
+print(Str[::-1])
+
+# Finding existence of an element
+print(Str.find("m"))   # returns index if found, -1 if not
+
+# Capitalizing the string
+print(Str.capitalize())
+
+# Finding index of an element 
+print(Str.index("u"))  # throws error if not found
+
+# Capitalizing every letter of a string
+print(Str.upper())
+
+# Finding length of a String
+print(len(Str))
+
