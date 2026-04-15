05ae4d71c0013a48f1d0b6d614ece98e61e570e9
Kunal Chachane <kunalchachane59@gmail.com>
2026-04-16 01:10:05 +0530

    Add more concepts of list to manipulating items of a list

--- List/ManipulatingList.py
+++ List/ManipulatingList.py
@@ -1,3 +1,19 @@
 List = ['Apple','Banana','Cherry']
 List[2] = 'Blueberry'
 print(List)
+
+List.append('Grape')
+print(List)
+
+List.insert(4,"Pineapple")
+print(List)
+
+List_2 = ['Pomegrenade','Kiwi']
+List.extend(List_2)
+print(List)
+
+List.remove("Kiwi")
+print(List)
+
+List.pop(2)
+print(List)
