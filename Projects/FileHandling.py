44c3827854de0aece1c087280494ccbb1588edf0
Kunal Chachane <kunalchachane59@gmail.com>
2026-04-27 22:43:06 +0530

    Fix indentation in readfilefromfolder function

--- Projects/FileHandling.py
+++ Projects/FileHandling.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 def readfilefromfolder():
-    path = Path('x')  # make sure folder 'x' exists
+    path = Path('x')  
     items = list(path.rglob('*'))
     for i, item in enumerate(items):
         print(f"{i+1} : {item}")
