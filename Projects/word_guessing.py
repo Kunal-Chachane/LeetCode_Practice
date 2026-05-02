37dd44f1403ebeea4feee54148335f9f72bcb1af
Kunal Chachane <kunalchachane59@gmail.com>
2026-05-02 18:55:52 +0530

    initial setup for word guessing game with difficulty modes

---
+++ Projects/word_guessing.py
@@ -0,0 +1,43 @@
+import random
+
+easy = [ "apple",
+         "cat",
+         "banana",
+         "grape",
+         "car"
+        ]
+
+medium = [  "zebra",
+            "green",
+            "india",
+            "Kiwi",
+            "May"
+         ]
+
+hard = [ "elephant",
+         "icecube",
+         "germany",
+         "pentaloon",
+         "pomegrenade"
+        ]
+
+
+print("Welcome to the word guessing game!")
+print("Select the difficulty mode (easy / medium / hard) \n")
+
+choice = input("select the mode: ").lower()
+
+if choice == easy: 
+    secret = choice.random(easy)
+elif choice == medium:
+    secret = choice.random(medium)
+elif choice == hard:
+    secret = choice.random(hard)
+else:
+    print("Invaid choice! selecting default easy mode")
+    secret = choice.random(easy)
+
+attempt = 0;
+print("Guess the sceret word")
+hint = ""
+
