8b29bf65dce7e4d6403e6a37373b8bad0ba60e7b
Kunal Chachane <kunalchachane59@gmail.com>
2026-05-28 23:42:29 +0530

    Initial commit: Proper folder structure

--- Temp_to_fahrenheit.py
+++
@@ -1,7 +0,0 @@
-# Write a program that will convert celsius value to fahrenheit
-
-tempInCelcius = int(input('Enter Temperature in Celsius: '))
-
-tempInFahrenheit = (9/5)*tempInCelcius + 32
-
-print(tempInFahrenheit)
