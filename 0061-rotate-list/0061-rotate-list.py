6058dbae205919faf4e6005ef2bc0692a32fc2a2
Kunal Chachane <kunalchachane59@gmail.com>
2026-07-09 23:07:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (48.23%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def rotateRight(self, head, k):
+        if not head or not head.next or k == 0:
+            return head
+
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k %= length
+        if k == 0:
+            return head
+
+        tail.next = head
+
+        new_tail = head
+        for i in range(length - k - 1):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
+
+        return new_head
