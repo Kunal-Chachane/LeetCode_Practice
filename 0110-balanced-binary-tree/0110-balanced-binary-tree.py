462b770a6cddf5dbf8ee299ef21a860865d7452c
Kunal Chachane <kunalchachane59@gmail.com>
2026-07-21 22:40:19 +0530

    Time: 8 ms (32.35%), Space: 17.3 MB (85.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def isBalanced(self, root):
-        if root == None:
-            return True
-        left = self.height(root.left)
-        right = self.height(root.right)
-        if abs(left - right) > 1:
-            return False
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+        return self.height(root) != -1
 
     def height(self, root):
         if root == None:
             return 0
         left = self.height(root.left)
         right = self.height(root.right)
+
+        if left == -1 or right == -1:
+            return -1
+
+        if abs(left - right) > 1:
+            return -1
         return 1 + max(left, right)
